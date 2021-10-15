718667b1c1fbc469d8a24dd0182e75d3cabee5d9
Victor Shevchuk <v.shevchuk@viseven.com>
2021-10-15 09:55:28 +0400

    Updates

---
+++ precache-manifest.f731b82d7a0777e59472b918343de6cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ef04cc1db327d472ed9bf6604b641b",
+    "url": "/react-http/index.html"
+  },
+  {
+    "revision": "bde619979b79d85535ac",
+    "url": "/react-http/static/css/main.c244030f.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/react-http/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-http/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde619979b79d85535ac",
+    "url": "/react-http/static/js/main.cf4ec0b8.chunk.js"
+  },
+  {
+    "revision": "2c58965eb74aa3199329",
+    "url": "/react-http/static/js/runtime-main.2a60e502.js"
+  }
+]);
